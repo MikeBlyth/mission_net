0f645eca8c03ec6269058d2dc0cbc54ce1ee1e63
Mike Blyth <mike.blyth@sim.org>
2012-06-02 17:53:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joslink::Application.config.secret_token = '6fa4809a8ecb5dbbd407382fcd461511d9d8830f9e4ecf56b1aae12f17a001ee04b62bb3b645ae10232fd7c1f80d76dd367c69e914df7cb17b26a2cd03aaabc3'
