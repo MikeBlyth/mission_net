825240b29d62d3fc47b716a547ef4fd75a057ea5
Mike Blyth <mike.blyth@sim.org>
2012-07-13 22:22:05 +0100

    in progress

--- app/controllers/app_logs_controller.rb
+++ app/controllers/app_logs_controller.rb
@@ -2,7 +2,7 @@
 class AppLogsController < ApplicationController
   # GET /app_logs
   # GET /app_logs.json
-  active_scaffold :app_logs do |config|
+  active_scaffold :app_log do |config|
   end
 
 end
