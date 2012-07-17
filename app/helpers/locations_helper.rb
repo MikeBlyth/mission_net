1a7326b47c164f798e51178af82ca32a02553ccc
Mike Blyth <mike.blyth@sim.org>
2012-07-17 20:02:47 +0100

    in progress

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -6,38 +6,11 @@ module LocationsHelper
       return s
     end
 
-  # TODO: May be best to replace this with the Rails method that does the same thing.
-  # http://api.rubyonrails.org/classes/ActionView/Helpers/FormOptionsHelper.html#method-i-option_groups_from_collection_for_select
-  def options_for_select_with_grouping(option_list, grouping_column, selected, value_column=:id, label_column=:description)
-    options = ["<option value=''></option>"]
-    groups = option_list.group_by{|opt| opt[grouping_column]}.sort_by { |k,v| k}
-    groups.each do |group|
-      group_label = group[0]
-      options << "<optgroup label='#{group_label}'>"
-      group_options = group[1].sort_by {|opt| opt[label_column]}
-      group_options.each do |option|
-        
-        if option[value_column] == selected
-          options <<  "<option value='#{option[value_column]}' selected='selected'>#{option[label_column]}<\/option>"
-        else
-          options <<  "<option value='#{option[value_column]}'>#{option[label_column]}<\/option>"
-        end
-      end
-    end
-    options
-  end
-
-  def location_choices(selected=999999)
+  def location_choices(selected)
   cities = City.where(true).order('name')
   return "<option value=''></option>" + 
       option_groups_from_collection_for_select(cities, :locations_sorted, 
           :name, :id, :description, selected)
-  #  selections =  Location.select("id, city_id, description")
-  #  hashed_locations = []
-  #  selections.each do | selection |
-  #    hashed_locations << {:id => selection.id, :city=>selection.city.name, :description => selection.description}
-  #  end
-  #  options_for_select_with_grouping(hashed_locations, :city, selected)
   end
 
 end # Module
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,19 @@
 class City < ActiveRecord::Base
-  attr_accessible :latitude, :longitude, :name
+  include ModelHelper
+  before_destroy :check_for_linked_records
+
   has_many :locations
+  validates_presence_of :description
+  validates_numericality_of :longitude, :allow_nil => true;
+  validates_numericality_of :latitude, :allow_nil => true;
+  # note we can't simply check for uniqueness of name since there can be cities w same name
+
+  def locations_sorted
+    locations.sort_by {|x| x.description}
+  end
+  
+  def to_s
+    self.name
+  end
+  
 end
