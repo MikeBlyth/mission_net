b952a5f348d8017b52a04173e36d19c7d35fabe8
Mike Blyth <mike.blyth@sim.org>
2012-07-18 22:16:56 +0100

    in progress

--- app/controllers/app_logs_controller.rb
+++ app/controllers/app_logs_controller.rb
@@ -1,8 +1,9 @@
-
 class AppLogsController < ApplicationController
-  # GET /app_logs
-  # GET /app_logs.json
-  active_scaffold :app_log do |config|
-  end
+#  include AuthenticationHelper
+#  load_and_authorize_resource
 
+  active_scaffold :app_log do |config|
+    config.columns = [:created_at, :severity, :code, :description]
+    list.sorting = {:created_at => 'DESC'}
+  end
 end
