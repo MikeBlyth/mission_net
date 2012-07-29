5528979fdbb51b6d6032f7e7b737fd15c128fa53
Mike Blyth <mike.blyth@sim.org>
2012-07-29 10:20:46 +0100

    in progress

--- app/models/message.rb
+++ app/models/message.rb
@@ -24,6 +24,7 @@
 #
 
 include MessagesHelper
+include SmsGatewayHelper
 
 class Message < ActiveRecord::Base
   attr_accessible :body, :code, :confirm_time_limit, :expiration, :following_up, :from_id, 
