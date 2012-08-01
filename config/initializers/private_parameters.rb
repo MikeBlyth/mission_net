d69a8e83e9ab34195b918cd9d57d7d6a1bcffcd1
Mike Blyth <mike.blyth@sim.org>
2012-08-01 18:56:50 +0100

    Adding back old omniauth.rb

--- config/initializers/private_parameters.rb
+++ config/initializers/private_parameters.rb
@@ -27,17 +27,17 @@ ActionMailer::Base.smtp_settings = {
   :openssl_verify_mode => 'none'
 }  
 
-OmniAuth.config.full_host = "http://localhost:3000" unless Rails.env == 'production' 
+#OmniAuth.config.full_host = "http://localhost:3000" unless Rails.env == 'production' 
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '468716646474464', '3c8e5e8fc55cef0a9efc833641fb8776'
-# If you don't need a refresh token -- if you're only using Google for account creation/auth and don't need google services -- set the access_type to 'online'.
-  # Also, set the approval prompt to an empty string, since otherwise it will be set to 'force', which makes users manually approve to the Oauth every time they log in.
-  # See http://googleappsdeveloper.blogspot.com/2011/10/upcoming-changes-to-oauth-20-endpoint.html
-  provider :google_oauth2, '110446490946.apps.googleusercontent.com', 'vsTJ7b2JVCM_xb85lnaAHz5w', 
-    {:scope => 'https://www.googleapis.com/auth/userinfo.email', :access_type => 'online', :approval_prompt=> ''}
-  provider :youtube, '110446490946.apps.googleusercontent.com', 'vsTJ7b2JVCM_xb85lnaAHz5w', 
-    {:scope => 'https://www.googleapis.com/auth/userinfo.email', :access_type => 'online', :approval_prompt=> ''}
+#Rails.application.config.middleware.use OmniAuth::Builder do
+#  provider :facebook, '468716646474464', '3c8e5e8fc55cef0a9efc833641fb8776'
+## If you don't need a refresh token -- if you're only using Google for account creation/auth and don't need google services -- set the access_type to 'online'.
+#  # Also, set the approval prompt to an empty string, since otherwise it will be set to 'force', which makes users manually approve to the Oauth every time they log in.
+#  # See http://googleappsdeveloper.blogspot.com/2011/10/upcoming-changes-to-oauth-20-endpoint.html
+#  provider :google_oauth2, '110446490946.apps.googleusercontent.com', 'vsTJ7b2JVCM_xb85lnaAHz5w', 
+#    {:scope => 'https://www.googleapis.com/auth/userinfo.email', :access_type => 'online', :approval_prompt=> ''}
+#  provider :youtube, '110446490946.apps.googleusercontent.com', 'vsTJ7b2JVCM_xb85lnaAHz5w', 
+#    {:scope => 'https://www.googleapis.com/auth/userinfo.email', :access_type => 'online', :approval_prompt=> ''}
 
-#  provider :google, '110446490946.apps.googleusercontent.com', 'vsTJ7b2JVCM_xb85lnaAHz5w'
+##  provider :google, '110446490946.apps.googleusercontent.com', 'vsTJ7b2JVCM_xb85lnaAHz5w'
 end
