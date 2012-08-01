38ef8dd165b9d8c4dd7f36e8171b07d61f575fd9
Mike Blyth <mike.blyth@sim.org>
2012-08-01 23:25:03 +0100

    env reader

--- config/initializers/private_parameters.rb
+++ config/initializers/private_parameters.rb
@@ -4,10 +4,16 @@
 
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
+if Rails.env == 'development'
+  File.open("#{Rails.root}/.env") do |file|
+    file.each_line do |line|
+      if line =~ /(.*?)=(.*)/
+        ENV[$1] = $2
+      end
+    end
+  end
+end
+
 Joslink::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
 
 # Action Mailer SMTP Settings
