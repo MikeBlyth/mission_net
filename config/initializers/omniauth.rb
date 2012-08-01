582445f532280f96e44de266bd1dd87c5341b816
Mike Blyth <mike.blyth@sim.org>
2012-08-01 20:08:12 +0100

    in progress

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -15,13 +15,15 @@ google_id = ENV['OMNIAUTH_GOOGLE_1']
 google_key = ENV['OMNIAUTH_GOOGLE_2']
 puts "**** google_id=#{google_id}"
 if google_key == 'vsTJ7b2JVCM_xb85lnaAHz5w'
+                  vsTJ7b2JVCM_xb85lnaAHz5w
   puts "**** Match ****"
 else
   puts "**** " + google_key
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['OMNIAUTH_FB_1'], ENV['OMNIAUTH_FB_2']
+  provider :facebook, '468716646474464', '3c8e5e8fc55cef0a9efc833641fb8776'
+#  provider :facebook, ENV['OMNIAUTH_FB_1'], ENV['OMNIAUTH_FB_2']
   provider :google_oauth2, google_id, google_key, 
     {:scope => 'https://www.googleapis.com/auth/userinfo.email', :access_type => 'online', :approval_prompt=> ''}
 end
