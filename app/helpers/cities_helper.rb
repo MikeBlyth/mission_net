46b2ae1f87f02dee0066e4a1486d74dcbe452b94
Mike Blyth <mjblyth@gmail.com>
2012-08-28 13:04:34 +0100

    Tweaking UI.

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -1,6 +1,6 @@
 module CitiesHelper
   def column_show_add_new(column, associated, record)
-    return false
+ #   return false
           true if column != :personnel_data &&
                   column != :health_data 
   end
